0b210d305c62526db4ab9302c4a7a91a1ba459f7
wjfwp <sis6710@gmail.com>
2023-10-24 22:17:44 +0900

    23/10/24

---
+++ BootBasic/src/test/java/com/simple/basic/JDBCTest.java
@@ -0,0 +1,14 @@
+package com.simple.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest //스프링 부트 테스트 클래스
+public class JDBCTest {
+	
+	@Test
+	public void testCode01() {
+		System.out.println(1);
+	}
+
+}
